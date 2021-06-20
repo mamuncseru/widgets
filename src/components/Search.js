5dea11eefaab7b4fd5441c956940e2d1d4b40fa6
mamuncseru <mamun.cseian@gmail.com>
2021-06-20 20:37:30 +0600

    Wikipedia Searching completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
-    console.log(results);
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -15,7 +24,7 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
 
@@ -23,12 +32,21 @@ const Search = () => {
         };
 
         search();
+    }, [debouncedTerm]);
 
-    }, [term]);
+   
 
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
